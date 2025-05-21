b5577d23c3ed573b68ab78f8636480ad28f83196
Théo Sommacal <t.somm@yahoo.fr>
2025-05-21 15:20:09 +0200

    Tests suite mentorat

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -23,11 +23,14 @@ function displayWork (work) {
     // Création de l'élément "figure"
     let figureElement = document.createElement("figure");
 
+    // Création d'une classe "fig"
+    figureElement.classList.add("fig")
+
     // Ajouter l'attribut "id" à l'élément "figureElement"
     figureElement.setAttribute("id", work.id);
 
     // Ajout attribut personnalisé "categoryID" à l'élément "figureElement"
-    figureElement.dataset.categoryId = work.categoryId;
+    figureElement.dataset.cat = work.categoryId;
 
     // Création de l'élément image
     const imageElement = document.createElement("img");
@@ -45,7 +48,23 @@ function displayWork (work) {
     // Insérer l'élément figure dans la section "gallery"
     sectionGallery.appendChild(figureElement);
 }
+// Fonction asynchrone pour récup les catégories (idem getWorks)
+async function getCategories () {
+    try {
+       const response = await fetch ("http://localhost:5678/api/categories")
+       console.log(response)
+       if (!response.ok) {
+        throw new Error ("Une erreur " + response.status + " s'est produite")
+       }
+       const categories = await response.json ()
+        displayCategories (categories)
+    }
+    catch (error) {alert(error)}
+}
 
+function displayCategories (categories) {
+
+   const filters = document.querySelector(".filters") 
 // Créer un bouton pour chaque catégorie
         for (let category of categories) {
             // Création du bouton
@@ -57,15 +76,8 @@ function displayWork (work) {
             // Ajouter la classe CSS
             boutonFiltreElement.classList.add("boutonFiltre");
 
-            // Sélectionner le bouton filtre selon la catégorie
-            if (category.id === filterCategoryId)
-                boutonFiltreElement.classList.add("boutonFiltreActive");
-
             // Définir le texte du bouton
             boutonFiltreElement.textContent = category.name;
-
-            // Ajouter le bouton dans la barre des filtres
-            boutonsFiltreElement.appendChild(boutonFiltreElement);
 
             // Créer une fonction d'évènement "click" pour le bouton
             boutonFiltreElement.addEventListener("click", function (event) {
@@ -73,8 +85,58 @@ function displayWork (work) {
                 const id = Number(event.target.dataset.id);
 
                 // Sélectionner le bouton filtre en fonction de la catégorie
-                selectionButtonFilter(id);
+                // selectionButtonFilter(id);
 
                 // Appliquer le filtre en fonction de l'identifiant catégorie
                 applyFilter(id);
             });
+
+            filters.appendChild(boutonFiltreElement)
+        }
+}
+
+getCategories()
+
+function applyFilter (id) {
+console.log(id)
+const allClass = document.getElementsByClassName("fig")
+for (let element of allClass) {
+    console.log(element.dataset.cat)
+    if (id != 0) {
+        console.log(parseInt(element.dataset.cat))
+         if (parseInt(element.dataset.cat) != id)
+       element.classList.add("hid")
+    else {
+        if (element.classList.contains("hid")) {
+        element.classList.remove("hid")
+        }
+    }
+} else {
+    if (element.classList.contains("hid")) {
+        element.classList.remove("hid")
+        }
+}
+}
+
+}
+
+/**
+ * Sort projects by categs
+ * @param {Evenment} e
+ */
+const sortByCat = (e) => {
+    const allClass = document.getElementsByClassName("fig");
+    for (let elem of allClass) {
+        //id = 0 ==> categs ALL
+        if (e.target.id !== "0") {
+            if (elem.dataset.cat !== e.target.id) {
+                elem.classList.add("hid");
+            } else {
+                if (elem.classList.contains("hid"))
+                    elem.classList.remove("hid");
+            }
+        } else {
+            if (elem.classList.contains("hid")) elem.classList.remove("hid");
+        }
+    }
+};
