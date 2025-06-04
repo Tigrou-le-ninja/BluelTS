cce9deae6e98cac99bf6de73215a996562088a80
Théo Sommacal <t.somm@yahoo.fr>
2025-06-04 11:22:26 +0200

    Modification form login et fonction connexion ()

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,15 +1,46 @@
-const email = document.getElementById("email")
-const password = document.getElementById("password")
+const form = document.getElementById("login");
 
-let emailInput = email.value
-let passwordInput = password.value
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        connexion(emailInput.value, passwordInput.value);
-        console.log(emailInput.value, passwordInput.value);
-    });
+    let emailInput = document.getElementById("email").value;
+    let passwordInput = document.getElementById("password").value;
 
- function connexion(email,password) {
+    if (emailInput === "" || passwordInput === "") {
+        alert("Veuillez remplir tous les champs.");
+        return;
+    }
+    const user = {
+        email: emailInput,
+        password: passwordInput,
+    };
+    connexion(user);
+});
 
- }  
+async function connexion(user) {
+    try {
+        // Poster à l'API et attendre la réponse
+        const response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + localStorage.getItem("token")
+            },
+            body: JSON.stringify(user),
+        });
+        if (!response.ok) {
+            throw new Error(
+                "Une erreur " + response.status + " s'est produite"
+            );
+        }
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        } catch (error) {
+        alert(error);
+    }
+        (data) => {
+            localStorage.setItem("token", data.token); 
+            window.location.href = "./index.html"; 
+        }
+}
